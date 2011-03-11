f34c3b9c9d1154798539494d4cf70d0f6f46a1b0
tysonite <tysonite@b35bc7a6-0bd6-6437-dd7b-65e59c63c965>
2011-03-11 12:16:49 +0000

    Added ValueAssignment generation for BOOLEAN and OCTET STRING (Hex)
    git-svn-id: http://asn1-compiler.googlecode.com/svn/trunk@44 b35bc7a6-0bd6-6437-dd7b-65e59c63c965

--- compiler/src/gen/visitor/ValueGenerator.java
+++ compiler/src/gen/visitor/ValueGenerator.java
@@ -6,33 +6,89 @@ import parser.*;
 public class ValueGenerator extends DoNothingASTVisitor implements Generator {
 
    private ASTValueAssignment node = null;
-   private CodeBuilder builder = new CodeBuilder();
+   private final CodeBuilder builder = new CodeBuilder();
 
    public ValueGenerator(ASTValueAssignment node) {
       this.node = node;
    }
 
+   @Override
    public Object visit(ASTBuiltinType node, Object data) {
       return node.childrenAccept(this, data);
    }
 
+   @Override
    public Object visit(ASTIntegerType node, Object data) {
       builder.append("enum { ");
       builder.append(this.node.getFirstToken().toString()).append(" = ");
       return data;
    }
 
+   @Override
+   public Object visit(ASTBooleanType node, Object data) {
+      builder.append("#define ");
+      builder.append(this.node.getFirstToken().toString()).append(" ");
+      return data;
+   }
+
+   @Override
+   public Object visit(ASTOctetStringType node, Object data) {
+      builder.append("#define ");
+      builder.append(this.node.getFirstToken().toString()).append(" \"");
+      return data;
+   }
+
+   @Override
    public Object visit(ASTBuiltinValue node, Object data) {
       return node.childrenAccept(this, data);
    }
 
+   @Override
+   public Object visit(ASTBooleanValue node, Object data) {
+      final String value = node.getFirstToken().toString();
+      if ("true".equalsIgnoreCase(value)) {
+         builder.append("true");
+      } else if ("false".equalsIgnoreCase(value)) {
+         builder.append("false");
+      } else {
+         throw new GeneratorException("Incorrect value: " + value);
+      }
+      builder.newLine();
+      return data;
+   }
+
+   @Override
+   public Object visit(ASTHexString node, Object data) {
+      final String value = node.getFirstToken().toString();
+
+      for (int i = 0; i <= (value.length() / 2) - 2; ++i) {
+         final String hexNumber = String.copyValueOf(value.toCharArray(), ((i + 1) * 2) - 1, 2);
+         final String octalNumber = Integer.toOctalString(Integer.valueOf(hexNumber, 16));
+
+         builder.append("\\");
+         for (int j = octalNumber.length(); j < 3; ++j) {
+            builder.append('0');
+         }
+
+         builder.append(octalNumber);
+      }
+      builder.append("\"");
+      builder.newLine();
+      return data;
+   }
+
+   @Override
    public Object visit(ASTSignedNumber node, Object data) {
       builder.append(node.getFirstToken().toString()).append(" };").newLine();
       return data;
    }
 
    public void generate() {
+      builder.append("// ValueAssignment for ASN.1 value: ").append(node.getFirstToken().toString());
+      builder.newLine();
+
       node.childrenAccept(this, null);
+
       builder.newLine();
    }
 
