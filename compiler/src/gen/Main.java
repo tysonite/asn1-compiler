b636013cf073daeaf05c55e20de710431c5a88e0
tysonite <tysonite@b35bc7a6-0bd6-6437-dd7b-65e59c63c965>
2011-04-09 19:36:04 +0000

    compiler: use apache common-cli for command line options parsing
    git-svn-id: http://asn1-compiler.googlecode.com/svn/trunk@169 b35bc7a6-0bd6-6437-dd7b-65e59c63c965

---
+++ compiler/src/gen/Main.java
@@ -0,0 +1,71 @@
+package gen;
+
+import java.io.*;
+
+import org.apache.commons.cli.*;
+import parser.*;
+
+public class Main {
+
+   private final Options options = new Options();
+   private final Option outputDirectory = new Option("o", "output-directory", true,
+           "Output directory for generated C++ code");
+   private final Option inputFile = new Option("i", "input-asn1-file", true,
+           "ASN.1 grammar to process");
+   private final Option help = new Option("h", "help", false,
+           "ASN.1 compiler command line options");
+
+   private void printHelp() {
+      final HelpFormatter h = new HelpFormatter();
+      h.printHelp("asn1-compiler", options);
+   }
+
+   private boolean mandatoryOptionsPresent(CommandLine line) {
+      return line.hasOption(inputFile.getOpt()) && line.hasOption(outputDirectory.getOpt());
+   }
+
+   private void initOptions() {
+      options.addOption(outputDirectory);
+      options.addOption(inputFile);
+      options.addOption(help);
+   }
+
+   private void run(String args[]) {
+      initOptions();
+
+      try {
+         final CommandLineParser argsParser = new PosixParser();
+         final CommandLine line = argsParser.parse(options, args);
+
+         if (line.hasOption(help.getOpt()) || !mandatoryOptionsPresent(line)) {
+            printHelp();
+            return;
+         }
+
+         AsnParser parser = new AsnParser(new FileInputStream(
+                 line.getOptionValue(inputFile.getOpt())));
+         parser.Input();
+         System.out.println("ASN.1 file parsed successfully.");
+
+         if (null != parser) {
+            SimpleNode root = parser.getTreeRootNode();
+            root.dump("\t");
+
+            CPPCodeGenerator generator = new CPPCodeGenerator(root,
+                    line.getOptionValue(outputDirectory.getOpt()));
+            generator.generate();
+         }
+      } catch (org.apache.commons.cli.ParseException e) {
+         printHelp();
+      } catch (parser.ParseException e) {
+         System.out.println(e);
+      } catch (FileNotFoundException e) {
+         System.out.println(e);
+      }
+   }
+
+   public static void main(String args[]) {
+      Main main = new Main();
+      main.run(args);
+   }
+}
