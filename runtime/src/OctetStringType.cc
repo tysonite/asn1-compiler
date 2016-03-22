e52f1638f9022eac0e6326ccf33be146b6d73b69
tysonite <tysonite@gmail.com>
2016-03-22 14:46:01 +0300

    runtime: fix compiler warnings

--- runtime/src/OctetStringType.cc
+++ runtime/src/OctetStringType.cc
@@ -13,19 +13,19 @@ void OctetStringType::checkType(const ValueType& value) const
    std::for_each(_sizes.begin(), _sizes.end(),
       [this, &value](const SizesType::value_type& p)
    {
-      if (p.first == p.second && value.size() != p.first)
+      if (p.first == p.second && value.size() != static_cast<ValueType::size_type>(p.first))
       {
          throw ASN1Exception(toString() + " value size is not equal to size '"
             + utils::ntos(p.first) + "'");
       }
 
-      if (p.first >= 0 && value.size() < p.first)
+      if (p.first >= 0 && value.size() < static_cast<ValueType::size_type>(p.first))
       {
          throw ASN1Exception(toString() + " value size is less than minimum size '"
             + utils::ntos(p.first) + "'");
       }
 
-      if (p.second >= 0 && value.size() > p.second)
+      if (p.second >= 0 && value.size() > static_cast<ValueType::size_type>(p.second))
       {
          throw ASN1Exception(toString() + " value size is more than maximum size '"
             + utils::ntos(p.second) + "'");
